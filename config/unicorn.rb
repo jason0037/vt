b94e0dc6c1afa93c341bcc625e59149b759dcaa4
jason <jason0037@21cn.com>
2013-12-23 23:07:21 +0800

    bugfix

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,6 +3,14 @@ preload_app true
 timeout 30
 listen 5000
 
+require 'pathname'
+
+path = Pathname.new(__FILE__).realpath # 当前文件完整路径
+
+path = path.sub('/config/unicorn.rb', '')
+
+APP_PATH = path.to_s
+
 # unicorn 日志
 
 stderr_path APP_PATH + "/log/unicorn.stderr.log"
