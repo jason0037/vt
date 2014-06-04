9d5593c7d6ce8e0f41157a01f0d97c3128993b05
sceneryye <scenery.ye@gmail.com>
2014-06-04 17:18:20 +0800

    fix bug of products' spec_value cannot show

--- app/models/ecstore/good_spec.rb
+++ app/models/ecstore/good_spec.rb
@@ -4,5 +4,6 @@ class Ecstore::GoodSpec < Ecstore::Base
 	belongs_to :good,			:foreign_key=>"goods_id"
 	belongs_to :spec,			:foreign_key=>"spec_id"
 	belongs_to :spec_value, 	:foreign_key=>"spec_value_id"
+  belongs_to :product, :foreign_key=>"product_id"
 
 end
