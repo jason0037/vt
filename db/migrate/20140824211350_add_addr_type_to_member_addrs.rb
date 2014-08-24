f2b2ccbc40f9d493b14cb56858c8863afdb9f3b8
sceneryye <scenery.ye@gmail.com>
2014-08-24 22:43:25 +0800

    fix orders' bug

--- db/migrate/20140824211350_add_addr_type_to_member_addrs.rb
+++ db/migrate/20140824211350_add_addr_type_to_member_addrs.rb
@@ -2,6 +2,7 @@ class AddAddrTypeToMemberAddrs < ActiveRecord::Migration
   def change
     add_column :sdb_b2c_member_addrs, :addr_type, :int
   end
+
   def connection
     @connection = Ecstore::Base.connection
   end
