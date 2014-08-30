042249151231564fcef34d5d187424e22090f81e
sceneryye <scenery.ye@gmail.com>
2014-08-30 19:01:01 +0800

    add multi-vshops homepages

--- app/models/ecstore/public_account.rb
+++ app/models/ecstore/public_account.rb
@@ -1,10 +1,12 @@
 #encoding:utf-8
-#include WeixinRailsMiddleware::AutoGenerateWeixinTokenSecretKey
+
 class Ecstore::PublicAccount < Ecstore::Base
+  #include WeixinRailsMiddleware::AutoGenerateWeixinTokenSecretKey
   self.table_name = 'public_accounts'
   self.accessible_all_columns
 
   attr_accessor :weixin_secret_key,:weixin_token,:supplier_id
+  attr_accessible :weixin_secret_key,:weixin_token,:supplier_id
 
 
 end
