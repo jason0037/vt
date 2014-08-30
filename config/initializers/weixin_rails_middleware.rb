872e299c782696545b96be9c5ef456b9e0c3dbd4
sceneryye <scenery.ye@gmail.com>
2014-08-30 09:51:48 +0800

    add multi-vshops homepages

--- config/initializers/weixin_rails_middleware.rb
+++ config/initializers/weixin_rails_middleware.rb
@@ -12,10 +12,10 @@ WeixinRailsMiddleware.configure do |config|
   ## Here configure is for you DON'T WANT TO SAVE your public account into database ##
   # Or the other configure is fit for only one weixin public_account
   # If you config `weixin_token_string`, so it will directly use it
-  #config.weixin_token_string = 'c51c4d8f4e770c86ba1c3da4'
+  config.weixin_token_string = 'c51c4d8f4e770c86ba1c3da4'
   #config.weixin_token_string = 'c51c4d8f4e770c86ba1c4ad3'
   # using to weixin server url to validate the token can be trusted.
-  #config.weixin_secret_string = '47y5oBQ_z2SNZPI-sKyblpvJmugcqXbd'
+  config.weixin_secret_string = '47y5oBQ_z2SNZPI-sKyblpvJmugcqXbd'
   #config.weixin_secret_string = '47y5oBQ_z2SNZPI-sKyblpvJmugcbdXq'
 
 end
