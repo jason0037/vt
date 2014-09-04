eb3a77ee55cd37f10c1ccf3bf746542589a36a0a
daizhiyu <1005352295@qq.com>
2014-09-04 16:18:27 +0800

    modified:   app/controllers/store/orders_controller.rb 	modified:   app/views/patch/member_addrs/_form_manco_first.html.erb 	modified:   app/views/patch/member_addrs/_form_manco_second.html.erb 	modified:   app/views/store/orders/_mancoder_show.html.erb 	modified:   app/views/store/orders/mancoder_show.html.erb

--- app/controllers/store/orders_controller.rb
+++ app/controllers/store/orders_controller.rb
@@ -499,7 +499,7 @@ class Store::OrdersController < ApplicationController
   def mancoder_show
     if @user
   member_id=params[:id]
-  @orders =Ecstore::Order.paginate :page=>params[:page],        ###分页语句
+  @order =Ecstore::Order.paginate :page=>params[:page],        ###分页语句
                                 :per_page => 10,
                                 :conditions => ["member_id=#{member_id}"]
     render :layout => "manco_template"
