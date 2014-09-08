cea86637ed8ff6ca58c9869363ce23f54abb208b
sceneryye <scenery.ye@gmail.com>
2014-09-08 16:47:06 +0800

    commissions

--- app/controllers/commissions_controller.rb
+++ app/controllers/commissions_controller.rb
@@ -86,6 +86,7 @@ group by left(FROM_UNIXTIME(alttime),7),recommend_user,supplier_id"
       supplier_id =78
     end
     @supplier =   Ecstore::Supplier.find(supplier_id)
+
     render :layout=>@supplier.layout
   end
 
