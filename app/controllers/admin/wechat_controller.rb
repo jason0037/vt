364653fd7342e54eabd60d1bc430f05b068c9589
sceneryye <scenery.ye@gmail.com>
2014-09-08 20:56:47 +0800

    commissions

--- app/controllers/admin/wechat_controller.rb
+++ app/controllers/admin/wechat_controller.rb
@@ -170,7 +170,7 @@ module Admin
            {
                "type":"view",
                "name":"我的订单",
-               "url":"http://www.trade-v.com/goods?platform=mobile"
+               "url":"http://www.trade-v.com/goods?platform=mobile&supplier_id=97"
             },
 
             {
