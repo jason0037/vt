02f4e13e2141b4e79a3886ac4625626b0d864e3e
sceneryye <scenery.ye@gmail.com>
2014-09-08 09:39:03 +0800

    commissions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
 
     #redirect_uri = "http://www.trade-v.com/auth/weixin/callback?id=#{@supplier.id}"
     #redirect_uri= URI::escape(redirect_uri)
-    redirect_uri="http%3a%2f%2fwww.trade-v.com%2fauth%2fweixin%2fcallback%3fid%3d@supplier.id}"
+    redirect_uri="http%3a%2f%2fwww.trade-v.com%2fauth%2fweixin%2fcallback%3fid%3d#{@supplier.id}}"
 
     @oauth2_url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=#{@supplier.weixin_appid}&redirect_uri=#{redirect_uri}&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect"
     session[:return_url] = params[:return_url]
