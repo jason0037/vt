6676259e727f51db779d2520e2f23ff6a969eb5d
daizhiyu <1005352295@qq.com>
2014-09-28 15:13:20 +0800

    modified:   app/controllers/vshop_controller.rb 	modified:   app/views/store/orders/norsh_show_order.html.erb

--- app/controllers/vshop_controller.rb
+++ app/controllers/vshop_controller.rb
@@ -196,55 +196,55 @@ end
     @paySign = ''    #微信签名
 
 
-    string sp_billno = Request["order_no"];
+    string sp_billno = Request["order_no"]
     #当前时间 yyyyMMdd
-    string date = DateTime.Now.ToString("yyyyMMdd");
+    string date = DateTime.Now.ToString("yyyyMMdd")
 
     if (null == sp_billno)
-      {
+
           #生成订单10位序列号，此处用时间和随机数生成，商户根据自己调整，保证唯一
-      sp_billno = DateTime.Now.ToString("HHmmss") + TenpayUtil.BuildRandomStr(4);
-      }
+      sp_billno = DateTime.Now.ToString("HHmmss") + TenpayUtil.BuildRandomStr(4)
+
     else
-      {
-          sp_billno = Request["order_no"].ToString();
-      }
+
+          sp_billno = Request["order_no"].ToString()
+
 
       sp_billno = TenpayUtil.partner + sp_billno;
 
 
 
       #创建支付应答对象
-      RequestHandler packageReqHandler = new RequestHandler(Context);
+      RequestHandler packageReqHandler = new RequestHandler(Context)
       #初始化
-      packageReqHandler.init();
+      packageReqHandler.init()
 
 
       #设置package订单参数
-      packageReqHandler.setParameter("partner", TenpayUtil.partner);		  #商户号
-      packageReqHandler.setParameter("fee_type", "1");                    #币种，1人民币
-      packageReqHandler.setParameter("input_charset", "GBK");
-      packageReqHandler.setParameter("out_trade_no", sp_billno);		#商家订单号
-      packageReqHandler.setParameter("total_fee", "1");			        #商品金额,以分为单位(money * 100).ToString()
-      packageReqHandler.setParameter("notify_url", TenpayUtil.tenpay_notify);		    #接收财付通通知的URL
-      packageReqHandler.setParameter("body", "JSAPIdemo");	                    #商品描述
-      packageReqHandler.setParameter("spbill_create_ip", Page.Request.UserHostAddress);   #用户的公网ip，不是商户服务器IP
+      packageReqHandler.setParameter("partner", TenpayUtil.partner)		  #商户号
+      packageReqHandler.setParameter("fee_type", "1")                   #币种，1人民币
+      packageReqHandler.setParameter("input_charset", "GBK")
+      packageReqHandler.setParameter("out_trade_no", sp_billno)		#商家订单号
+      packageReqHandler.setParameter("total_fee", "1")		        #商品金额,以分为单位(money * 100).ToString()
+      packageReqHandler.setParameter("notify_url", TenpayUtil.tenpay_notify)		    #接收财付通通知的URL
+      packageReqHandler.setParameter("body", "JSAPIdemo")	                    #商品描述
+      packageReqHandler.setParameter("spbill_create_ip", Page.Request.UserHostAddress)   #用户的公网ip，不是商户服务器IP
 
       #获取package包
-      packageValue = packageReqHandler.getRequestURL();
+      packageValue = packageReqHandler.getRequestURL()
 
       #调起微信支付签名
-      timeStamp = TenpayUtil.getTimestamp();
-      nonceStr = TenpayUtil.getNoncestr();
+      timeStamp = TenpayUtil.getTimestamp()
+      nonceStr = TenpayUtil.getNoncestr()
 
       #设置支付参数
-      RequestHandler paySignReqHandler = new RequestHandler(Context);
-      paySignReqHandler.setParameter("appid", appId);
-      paySignReqHandler.setParameter("appkey", TenpayUtil.appkey);
-      paySignReqHandler.setParameter("noncestr", nonceStr);
-      paySignReqHandler.setParameter("timestamp", timeStamp);
-      paySignReqHandler.setParameter("package", packageValue);
-      paySign = paySignReqHandler.createSHA1Sign();
+      RequestHandler paySignReqHandler = new RequestHandler(Context)
+      paySignReqHandler.setParameter("appid", appId)
+      paySignReqHandler.setParameter("appkey", TenpayUtil.appkey)
+      paySignReqHandler.setParameter("noncestr", nonceStr)
+      paySignReqHandler.setParameter("timestamp", timeStamp)
+      paySignReqHandler.setParameter("package", packageValue)
+      paySign = paySignReqHandler.createSHA1Sign()
 
 
 
@@ -254,5 +254,5 @@ end
       #string paySignDebuginfo = paySignReqHandler.getDebugInfo();
       #Response.Write("<br/>paySignDebuginfo:" + paySignDebuginfo + "<br/>");
   end
-
+end
 end
