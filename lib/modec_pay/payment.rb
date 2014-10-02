eb9210a1ee5e0c98b06df92ebb94b74b53718067
sceneryye <scenery.ye@gmail.com>
2014-10-03 07:50:24 +0800

    wxpay

--- lib/modec_pay/payment.rb
+++ lib/modec_pay/payment.rb
@@ -172,13 +172,11 @@ module ModecPay
 
 
                   if(res.err_msg == "get_brand_wcpay_request:ok" ) {
-window.location ="/vshop/78/paynotifyurl?temp=solution&payment_id=#{self.fields['out_trade_no']}"
-}
-else{
-alert(res.err_msg);
-}
-                  // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
-                  //因此微信团队建议，当收到ok返回时，向商户后台询问是否收到交易成功的通知，若收到通知，前端展示交易成功的界面；若此时未收到通知，商户后台主动调用查询订单接口，查询订单的当前状态，并反馈给前端展示相应的界面。
+                    window.location ="/vshop/78/paynotifyurl?temp=solution&payment_id=#{self.fields['out_trade_no']}";
+                  }
+                    else{alert('支付未成功');
+                    window.location ="/orders/norsh_show_order?id=#{@order.order_id}";
+                  }
               });
 
           });
