576964da2109aae49435b11b913dfeb83edd7ab4
daizhiyu <1005352295@qq.com>
2014-10-09 17:13:40 +0800

    modified:   app/controllers/sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -110,7 +110,7 @@ class SessionsController < ApplicationController
       # refer_url = "/" unless refer_url
        return_url= params[:return_url] unless return_url
       if params[:platform]=="mobile"
-        redirect_to "/mlogin?id=#{params[:id]}&return_url=#{return_url}"
+        redirect_to "/mlogin?id=#{params[:id]}&return_url=#{return_url}&supplier_id=#{params[:id]}"
       elsif
         redirect_to "/vshop"
       else
