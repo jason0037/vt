bf13e85819989c04fa68f8a7dcbb8b495d5da0bd
sceneryye <scenery.ye@gmail.com>
2014-10-22 04:46:59 +0800

    add multi-freight

--- app/controllers/store/orders_controller.rb
+++ app/controllers/store/orders_controller.rb
@@ -415,7 +415,7 @@ class Store::OrdersController < ApplicationController
     end
 
    sql = "SELECT SUM(price*quantity) AS total,mdk.sdb_b2c_cart_objects.supplier_id,SUM(freight)/count(*) AS freight FROM mdk.sdb_b2c_cart_objects
-INNER JOIN mdk.sdb_b2c_goods ON SUBSTRING(SUBSTRING_INDEX(mdk.sdb_b2c_cart_objects.obj_ident,'_',2),'_',-1) = mdk.sdb_b2c_goods.product_id
+INNER JOIN mdk.sdb_b2c_goods ON SUBSTRING(SUBSTRING_INDEX(mdk.sdb_b2c_cart_objects.obj_ident,'_',2),'_',-1) = mdk.sdb_b2c_goods.goods_id
 WHERE mdk.sdb_b2c_cart_objects.member_id=#{@user.member_id}
 GROUP BY mdk.sdb_b2c_cart_objects.supplier_id"
     @cart_total_by_supplier = ActiveRecord::Base.connection.execute(sql)
