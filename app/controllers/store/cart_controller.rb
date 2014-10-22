2fa980c5e59bf277d9869ed5e7370ed49ed6bed8
daizhiyu <1005352295@qq.com>
2014-10-22 23:31:19 +0800

    modified:   app/controllers/store/cart_controller.rb 	modified:   app/views/store/cart/mobile.html.erb 	modified:   app/views/store/goods/mproduct.html.erb

--- app/controllers/store/cart_controller.rb
+++ app/controllers/store/cart_controller.rb
@@ -9,6 +9,7 @@ class Store::CartController < ApplicationController
 
   def mobile
     supplier_id=params[:id]
+    @line_items = Ecstore::Cart.where(:member_id=>params[:user_id]).order("supplier_id")
     good_supplier_id = params[:good_supplier_id]
     @goods_supplier = 0
     @bg_color = ["#cde6f3","#e5fdff"]
@@ -101,7 +102,7 @@ class Store::CartController < ApplicationController
       supplier_id = 78
     end
     if params[:platform]=='mobile'
-      redirect_to "/cart/mobile?id=#{supplier_id}"
+      redirect_to "/cart/mobile?id=#{supplier_id}&user_id=#{params[:user_id]}"
       #render "mobile", :layout=>@supplier.layout
     else
       render "add"
@@ -123,7 +124,7 @@ class Store::CartController < ApplicationController
 		_type, goods_id, product_id = params[:id].split('_')
 		@line_items.where(:obj_ident=>params[:id]).delete_all
 		@user.custom_specs.where(:product_id=>product_id).delete_all if signed_in?
-    @line_items  = @line_items.where(:obj_ident=>params[:id]).first
+    @line_items = Ecstore::Cart.where(:member_id=>params[:user_id]).order("supplier_id")
 		find_cart!
   #  if params[:platform]=='mobile'
   #    return  render :text=>"删除成功"# redirect_to "/cart/mobile"
