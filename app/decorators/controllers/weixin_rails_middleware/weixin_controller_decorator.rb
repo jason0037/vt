65173b9b5dba82eb7ffe49b4e31ce07c18e4d5e3
sceneryye <scenery.ye@gmail.com>
2014-10-27 08:16:23 +0800

    fix bug

--- app/decorators/controllers/weixin_rails_middleware/weixin_controller_decorator.rb
+++ app/decorators/controllers/weixin_rails_middleware/weixin_controller_decorator.rb
@@ -63,7 +63,7 @@ WeixinRailsMiddleware::WeixinController.class_eval do
         title="一大波螃蟹正在接近-天山蟹客"
         desc ="天山蟹客大闸蟹实行人放养的野生自然成长程序，以湖中的水草、生物为食。这里冬季水温较低，螃蟹需要2-3年才可以成长3两重以上的商品蟹。雪水补给的湖泊中还含有千分之五的盐碱，低温高盐环境下养出的螃蟹身体十分结实，肉质也十分细嫩。"
         pic_url="http://vshop.trade-v.com/images/a087/crap.jpg"
-        link_url="http://vshop.trade-v.com/mgallery?name=%E5%A4%A9%E5%B1%B1%E5%A4%A7%E9%97%B8%E8%9F%B9&id=#{id}&supplier_id=#{id}"
+        link_url="http://vshop.trade-v.com/mgallery?name=%E5%A4%A9%E5%B1%B1%E5%A4%A7%E9%97%B8%E8%9F%B9&id=#{id}&supplier_id=#{id}&from=weixin&wechatuser=#{user}"
         articles = [generate_article(title, desc, pic_url, link_url)]
         reply_news_message(articles)
 
