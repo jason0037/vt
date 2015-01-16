83ef8ba94e99cfa2f9f0f2d9876d55368a761d9e
daizhiyu <1005352295@qq.com>
2015-01-16 23:11:44 +0800

    modified:   app/controllers/admin/sessions_controller.rb

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -9,17 +9,26 @@ class Admin::SessionsController < Admin::BaseController
   def create
   	name  = params[:session][:login_name]
   	password = params[:session][:login_password]
-    
+
       admin =  Ecstore::Account.admin_authenticate(name,password)
       if admin
             admin_sign_in admin
           unless params[:return_url].nil?
             redirect_to params[:return_url]
-        else
-  		 redirect_to after_admin_sign_in_path
+          else
+            if params[:platform]=="vshop"
+              redirect_to "/vshop/login" ,:notice=>"不是供应商"
+            else
+  		   redirect_to after_admin_sign_in_path
+              end
           end
       else
-  		redirect_to new_admin_session_path,:notice=>"用户名或密码错误"
+        if params[:platform]=="vshop"
+          redirect_to "/vshop/login" ,:notice=>"用户名或密码错误"
+        else
+          redirect_to new_admin_session_path,:notice=>"用户名或密码错误"
+        end
+
   	end
   end
   	
