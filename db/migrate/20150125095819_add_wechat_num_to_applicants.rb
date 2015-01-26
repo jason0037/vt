53f1be4819bc3df941d89b79f29cb709161977ed
daizhiyu <1005352295@qq.com>
2015-01-26 23:58:08 +0800

    modified:   app/views/events/foodie/_form.html.erb

--- db/migrate/20150125095819_add_wechat_num_to_applicants.rb
+++ db/migrate/20150125095819_add_wechat_num_to_applicants.rb
@@ -2,7 +2,7 @@ class AddWechatNumToApplicants < ActiveRecord::Migration
   def self.up
     change_table :applicants do |t|
       t.string :member_id
-      t.text :user_desc
+
     end
   end
 end
