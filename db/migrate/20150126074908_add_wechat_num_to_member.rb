7886694dd7840e4a1847a34a4d21c3b3ad5b33bb
daizhiyu <1005352295@qq.com>
2015-01-26 23:41:58 +0800

    new file:   db/migrate/20150126074908_add_wechat_num_to_member.rb

---
+++ db/migrate/20150126074908_add_wechat_num_to_member.rb
@@ -0,0 +1,10 @@
+class AddWechatNumToMember < ActiveRecord::Migration
+  def self.up
+    change_table :sdb_b2c_members do |t|
+      t.string :wechat_num   ###微信号
+    end
+  end
+  def connection
+    @connection = Ecstore::Base.connection
+  end
+end
