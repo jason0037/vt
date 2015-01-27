d20b18efe80d8df9ec4d66fed8db7a80d5551d2c
daizhiyu <1005352295@qq.com>
2015-01-27 11:33:27 +0800

    modified:   db/migrate/20150125093418_add_times_to_events.rb

--- db/migrate/20150125093418_add_times_to_events.rb
+++ db/migrate/20150125093418_add_times_to_events.rb
@@ -1,12 +1,12 @@
 class AddTimesToEvents < ActiveRecord::Migration
   def self.up
     change_table :events do |t|
-      # t.column :times, :timestamp
-      #
-      # t.string :adds   ###地点
-      #
-      # t.column :terminal ,"ENUM('pc','mobile')",:default=>"pc"
-      # t.integer :member_id
+      t.integer :startime
+
+      t.string :adds   ###地点
+
+      t.column :terminal ,"ENUM('pc','mobile')",:default=>"pc"
+      t.integer :member_id
       t.integer :endtime
 
     end
