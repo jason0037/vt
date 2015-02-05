af4bbadf6899711298e0eac5e9a1b4c68ae20813
daizhiyu <1005352295@qq.com>
2015-02-05 12:25:52 +0800

    modified:   app/views/layouts/shop.html.erb

--- app/controllers/shop/shopusers_controller.rb
+++ app/controllers/shop/shopusers_controller.rb
@@ -13,15 +13,48 @@ class Shop::ShopusersController < ApplicationController
     if params[:shop_id]
       shop_id = params[:shop_id]
     else
-      shop_id =Ecstore::Shop.find_by_member_id( @user.member_id).shop_id
+      shop_id =Ecstore::Shop.find_by_member_id(@user.member_id).shop_id
     end
 
      @shop= Ecstore::Shop.where(:shop_id=>shop_id,:status=>1).first
+
+    @share_for_shop = 0
+    @share_for_sale = 0
+    @share_for_promotion = 0
+
+    if @user.member_id.to_s==@shop.member_id.to_s
+
+      @share = Ecstore::Order.all(:conditions => "shop_id = #{ shop_id}",
+                                 :select => "SUM(share_for_sale) share_sale ,SUM(final_amount) final_amount",:group=>"shop_id").first
+      if @share
+        @share_for_sale = @share.share_sale
+      end
+      if @shop.parent.nil?
+        shop_ids = 0
+        results = Ecstore::Shop.find_by_parent(shop_id)
+        if results
+          results.each(:as => :hash) do |row|
+          end
+        end
+
+        @share_for_shop = Ecstore::Order.all(:conditions => "shop_id in (#{shop_ids}) and orderstatus='true'",
+                                             :select => "SUM(share_for_shop) share_shop, shop_id",:group=>"shop_id")
+      end
+    else
+      share = Ecstore::Order.all(:conditions => "shop_id = #{shop_id} and member_id=#{@user.member_id} and orderstatus='true'",
+                                 :select => "SUM(share_for_promotion) share_promotion",:group=>"shop_id,member_id").first
+      if share
+        @share_for_promotion = share.share_promotion
+      end
+
+    end
+
+
   end
 
  def clients
    @shop_title="客户管理"
-   @shop_id = Ecstore::Shop.find_by_member_id(@user.member_id).shop_id
+   @shop_id = @user.shops
    @shop_clients=Ecstore::ShopClient.where(:shop_id=>@user.member_id)
 
  end
