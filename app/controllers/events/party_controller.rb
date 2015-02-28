ee611960563e906a48510a32a54c570049f07291
Scenery Ye <scenery.ye@gmail.com>
2015-02-28 15:38:01 +0800

    get rid off notice of spring festival

--- app/controllers/events/party_controller.rb
+++ app/controllers/events/party_controller.rb
@@ -1,31 +1,28 @@
 #encoding:utf-8
 class Events::PartyController < ApplicationController
   layout "tradev"
+
   def index
     if @user
     @supplier=Ecstore::Supplier.find(params[:supplier_id])
     render :layout => "default"
    end
-
-
-
   end
 
   def stepone
-
     @users=Ecstore::User.where(:member_id=>@user.member_id).first
      if @users.update_attributes(params[:user])
 
      end
     redirect_to "/events/party?supplier_id=78&step=2"
-     end
+  end
 
-   def steptwo
-
-     @event = Imodec::Event.new(params[:event]) do |ev|
+  def steptwo
+    @event = Imodec::Event.new(params[:event]) do |ev|
        ev.startime= Time.parse(params[:event][:startime]).to_i
        ev.endtime= Time.parse(params[:event][:endtime]).to_i
-       end
+    end
+
      if @event.save
        redirect_to "/events/party?supplier_id=78&step=3"
      end
@@ -34,27 +31,24 @@ class Events::PartyController < ApplicationController
   def list
     @supplier=Ecstore::Supplier.find(params[:supplier_id])
     @events  = Imodec::Event.paginate(:page => params[:page], :per_page => 20,:order=>"endtime desc")
-
   end
 
   def user_party
-   @supplier=Ecstore::Supplier.find(params[:supplier_id])
+    @supplier=Ecstore::Supplier.find(params[:supplier_id])
     @events  = Imodec::Event.paginate(:page => params[:page], :per_page => 20,:order=>"endtime desc",:conditions =>{
         :terminal=>"mobile",:member_id=>@user.member_id})
-
   end
 
 
   def detail
-   
-
     @supplier=Ecstore::Supplier.find(params[:supplier_id])
     @event = Imodec::Event.find(params[:id])
     @applicants = @event.applicants.paginate(:page => params[:page], :per_page => 20,:order=>"created_at desc")
     @event_desc=@event.name
-     if params[:id]=='8'
+    #塞浦路斯
+    if params[:id]=='8'
       render :layout=>"cyprus"
     end
   end
 
-  end
+end
